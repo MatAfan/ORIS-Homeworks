45626b0ae1c287b772f6449759f32d821ae39bac
Mtvy <78172022+MatAfan@users.noreply.github.com>
2025-11-15 10:39:39 +0300

    Added first control work

---
+++ HappyDay/MigrationLibrary/MigrationSystem/DatabaseConnection.cs
@@ -0,0 +1,10 @@
+﻿namespace MigrationSystem;
+
+public class DatabaseConnection(string connectionString)
+{
+    private string connectionString = connectionString;
+    
+    private Dictionary<string, Dictionary<string, string>> tables = new();
+    
+    
+}
